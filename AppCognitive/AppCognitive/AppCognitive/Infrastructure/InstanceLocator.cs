154f0dbbd14fe018d5e2f126cca72a4224d57c55
Pedro Abel <angeluz-abel@hotmail.es>
2017-06-22 16:46:55 -0500

    clean app

---
+++ AppCognitive/AppCognitive/AppCognitive/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCognitive.Infrastructure
+{
+    public class InstanceLocator
+    {
+        public InstanceLocator()
+        {
+            Main = new ViewModel.CognitiveVM();
+        }
+        public ViewModel.CognitiveVM Main { get; set; }
+    }
+}
