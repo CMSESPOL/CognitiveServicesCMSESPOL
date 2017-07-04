a91ab3b2c85a2b6e557f7b595c5de623537693a2
Pedro Abel <angeluz-abel@hotmail.es>
2017-07-04 16:04:24 -0500

    funtional

--- AppCognitive/AppCognitive/AppCognitive/ViewModel/CognitiveVM.cs
+++ AppCognitive/AppCognitive/AppCognitive/ViewModel/CognitiveVM.cs
@@ -1,12 +1,4 @@
-﻿using AppCognitive.Views;
-using GalaSoft.MvvmLight.Command;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
+﻿using System.Collections.ObjectModel;
 
 namespace AppCognitive.ViewModel
 {
