882c6296ea7e9bf6067f81ec8e23b2e3ba237041
Pedro Abel <angeluz-abel@hotmail.es>
2017-07-05 09:19:15 -0500

    clean using

--- AppCognitive/AppCognitive/AppCognitive/Views/NewCapturePage.xaml.cs
+++ AppCognitive/AppCognitive/AppCognitive/Views/NewCapturePage.xaml.cs
@@ -3,12 +3,8 @@ using Microsoft.ProjectOxford.Emotion;
 using Plugin.Media;
 using Plugin.Media.Abstractions;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
