08dc34aab00dcd48724400db36e1780a86336c95
Pedro Abel <angeluz-abel@hotmail.es>
2017-07-05 09:17:00 -0500

    new declaration menu

--- AppCognitive/AppCognitive/AppCognitive/ViewModel/CognitiveVM.cs
+++ AppCognitive/AppCognitive/AppCognitive/ViewModel/CognitiveVM.cs
@@ -30,41 +30,43 @@ namespace AppCognitive.ViewModel
 
         private void LoadMenu()
         {
-            Menu = new ObservableCollection<MenuItemVM>();
-            Menu.Add(new MenuItemVM()
+            Menu = new ObservableCollection<MenuItemVM>
             {
-                Icon = "ic_add_photo",
-                Title = "Capturar",
-                PageName = "NewCapturePage"
-            });
+                new MenuItemVM()
+                {
+                    Icon = "ic_add_photo",
+                    Title = "Capturar",
+                    PageName = "NewCapturePage"
+                },
 
-            Menu.Add(new MenuItemVM()
-            {
-                Icon = "ic_select",
-                Title = "Seleccionar",
-                PageName = "SelectPage"
-            });
+                new MenuItemVM()
+                {
+                    Icon = "ic_select",
+                    Title = "Seleccionar",
+                    PageName = "SelectPage"
+                },
 
-            Menu.Add(new MenuItemVM()
-            {
-                Icon = "ic_resultCS",
-                Title = "Resultado",
-                PageName = "ResultPage"
-            });
+                new MenuItemVM()
+                {
+                    Icon = "ic_resultCS",
+                    Title = "Resultado",
+                    PageName = "ResultPage"
+                },
 
-            Menu.Add(new MenuItemVM()
-            {
-                Icon = "ic_share",
-                Title = "Compartir",
-                PageName = "SharePage"
-            });
+                new MenuItemVM()
+                {
+                    Icon = "ic_share",
+                    Title = "Compartir",
+                    PageName = "SharePage"
+                },
 
-            Menu.Add(new MenuItemVM()
-            {
-                Icon = "ic_us",
-                Title = "Nosotros",
-                PageName = "UsPage"
-            });
+                new MenuItemVM()
+                {
+                    Icon = "ic_us",
+                    Title = "Nosotros",
+                    PageName = "UsPage"
+                }
+            };
         }
     }
 }
